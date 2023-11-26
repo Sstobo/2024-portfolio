d48ea8c3ecf8199b19bbb0122ee3c295d7cefbe4
Sstobo <seanstobo@gmail.com>
2023-11-26 14:07:35 -0800

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <ScrollSection>
         <div className="w-12/12 h-screen">
             <h2 className="mb-5 xl:mb-24 text-5xl md:text-7xl xl:text-9xl  w-full  font-black text-transparent bg-clip-text bg-gradient-to-br from-teal-900 via-teal-500 to-purple-900 dark:from-amber-200 dark:via-amber-600 dark:to-amber-100">Lets get in touch!</h2>
-            <p className="w-full xl:w-7/12 md:text-3xl text-xl xl:text-5xl xl:mb-32 md:mb-24 mb-10 font-medium"> My mission is to harness the power of your web platforms to drive revenue, streamline costs, and equip businesses for the future. I'm committed to demystifying technology and aligning it with your business goals, ensuring your team thrives and your bottom line reflects our combined success.</p>
+            <p className="w-full xl:w-7/12 md:text-3xl text-xl xl:text-5xl xl:mb-32 md:mb-24 mb-10 font-medium"> My mission is to harness the power of your web platforms to drive revenue, streamline costs, and equip businesses for the future. I am committed to demystifying technology and aligning it with your business goals, ensuring your team thrives and your bottom line reflects our combined success.</p>
    
             <CopyToClipboard
                   text="seanstobo@gmail.com"
